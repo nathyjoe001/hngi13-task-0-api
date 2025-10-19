661b135c08f06a067917dabcd82577f7f6edceea
nathyjoe001 <akpannath84@gmail.com>
2025-10-19 22:54:33 +0100

    update requirements.txt

--- profile_api/settings.py
+++ profile_api/settings.py
@@ -15,8 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 # -------------------------
 # APPLICATIONS
 # -------------------------
