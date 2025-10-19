5aa24edfb782d72f5d120efcdab02c0eff0a0d64
nathyjoe001 <akpannath84@gmail.com>
2025-10-19 23:31:53 +0100

    update .env

--- profile_api/settings.py
+++ profile_api/settings.py
@@ -15,6 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 # -------------------------
 # APPLICATIONS
