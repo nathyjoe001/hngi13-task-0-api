92e9283b13f2a01a7cc8976e6ddbc8181191e4f7
nathyjoe001 <akpannath84@gmail.com>
2025-10-21 16:11:48 +0100

    Redirect root URL to /api/me

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -12,15 +12,19 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('api/', include('api.urls'))
 """
 
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+     # Redirect root "/" to "/api/me"
+    path('', lambda request: HttpResponseRedirect('/api/me')),
 ]
 
